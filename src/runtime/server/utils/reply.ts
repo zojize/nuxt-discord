a737849ecaf2e27233dc83700e3356127dd637a4
zojize <jeffzo01@qq.com>
2025-07-01 22:01:00 -0500

    feat: enhance reply function to support file handling

--- src/runtime/server/utils/reply.ts
+++ src/runtime/server/utils/reply.ts
@@ -1,19 +1,28 @@
-import type { InteractionEditReplyOptions, InteractionReplyOptions } from 'discord.js'
-import type { SlashCommandCustomReturnHandler } from '~/src/types'
+import type { BaseMessageOptions, InteractionEditReplyOptions, InteractionReplyOptions } from 'discord.js'
+import type { SlashCommandCustomReturnHandler } from '../../../types'
 import { MessageFlags } from 'discord.js'
 
 type OmitPreservingCallSignature<T, K extends keyof T = keyof T>
   = Omit<T, K> & (T extends (...args: infer R) => infer S ? (...args: R) => S : unknown)
-interface ReplyFunction {
-  (text: string, options?: InteractionReplyOptions): SlashCommandCustomReturnHandler
+
+type File = NonNullable<BaseMessageOptions['files']>[number]
+
+type ReplyFunction = SlashCommandCustomReturnHandler & {
+  (text?: string, options?: InteractionReplyOptions): SlashCommandCustomReturnHandler
   edit: EditReplyFunction
   ephemeral: OmitPreservingCallSignature<ReplyFunction, 'edit' | 'ephemeral'>
   flags: (flags: InteractionReplyOptions['flags']) => OmitPreservingCallSignature<ReplyFunction, 'edit'>
+  file: (file: File) => OmitPreservingCallSignature<ReplyFunction, 'edit'>
+  files: (files: File[]) => OmitPreservingCallSignature<ReplyFunction, 'edit'>
+  send: ReplyFunction
 }
 
-interface EditReplyFunction {
-  (text: string, options?: InteractionEditReplyOptions): SlashCommandCustomReturnHandler
+type EditReplyFunction = SlashCommandCustomReturnHandler & {
+  (text?: string, options?: InteractionEditReplyOptions): SlashCommandCustomReturnHandler
   flags: (flags: InteractionEditReplyOptions['flags']) => EditReplyFunction
+  file: (file: File) => EditReplyFunction
+  files: (files: File[]) => EditReplyFunction
+  send: EditReplyFunction
 }
 
 function createReplyFunction(
@@ -22,22 +31,40 @@ function createReplyFunction(
     | InteractionEditReplyOptions
   ) & { editReply?: boolean } = {},
 ): ReplyFunction {
-  const reply = (text: string, options?: InteractionReplyOptions | InteractionEditReplyOptions): SlashCommandCustomReturnHandler => async (interaction) => {
-    if (editReply) {
-      await interaction.editReply({
-        content: text,
-        ...(defaultOptions as InteractionEditReplyOptions),
-        ...(options as InteractionEditReplyOptions),
-      })
+  const reply = ((...args) => {
+    if (typeof args[0] === 'string' || typeof args[0] === 'undefined') {
+      const [text, options] = args
+      return async (interaction) => {
+        if (editReply || interaction.deferred || interaction.replied) {
+          await interaction.editReply({
+            content: text,
+            ...(defaultOptions as InteractionEditReplyOptions),
+            ...(options as InteractionEditReplyOptions),
+          })
+        }
+        else {
+          await interaction.reply({
+            content: text,
+            ...(defaultOptions as InteractionReplyOptions),
+            ...(options as InteractionReplyOptions),
+          })
+        }
+      }
     }
     else {
-      await interaction.reply({
-        content: text,
-        ...(defaultOptions as InteractionReplyOptions),
-        ...(options as InteractionReplyOptions),
-      })
+      const interaction = args[0]
+      if (editReply || interaction.deferred || interaction.replied) {
+        return interaction.editReply({
+          ...(defaultOptions as InteractionEditReplyOptions),
+        }).then(() => undefined)
+      }
+      else {
+        return interaction.reply({
+          ...(defaultOptions as InteractionReplyOptions),
+        }).then(() => undefined)
+      }
     }
-  }
+  }) as ReplyFunction
 
   Object.defineProperties(reply, {
     ephemeral: {
@@ -73,9 +100,28 @@ function createReplyFunction(
         })
       },
     },
+    file: {
+      get() {
+        return (file: File) => createReplyFunction({
+          ...defaultOptions,
+          files: defaultOptions.files != null ? [...defaultOptions.files, file] : [file],
+        })
+      },
+    },
+    files: {
+      get() {
+        return (files: File[]) => createReplyFunction({
+          ...defaultOptions,
+          files: defaultOptions.files != null ? [...defaultOptions.files, ...files] : files,
+        })
+      },
+    },
+    send: {
+      value: reply,
+    },
   })
 
-  return reply as ReplyFunction
+  return reply
 }
 
 export const reply: ReplyFunction = createReplyFunction()
