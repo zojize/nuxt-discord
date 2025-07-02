7ac3fb0c728284c4872515191b1114016d07db89
zojize <jeffzo01@qq.com>
2025-07-01 22:01:17 -0500

    test: enhance reply tests to cover file handling and multiple files

--- test/reply.test.ts
+++ test/reply.test.ts
@@ -1,14 +1,15 @@
 import type { ChatInputCommandInteraction } from 'discord.js'
+import type { DiscordClient } from '~/src/runtime/server/utils/client'
 import { MessageFlags } from 'discord.js'
 import { describe, expect, it, vi } from 'vitest'
 import { reply } from '../src/runtime/server/utils/reply'
 
 describe('reply', () => {
-  const MockClient = vi.fn()
+  const MockClient = vi.fn() as { new (): DiscordClient }
   const mockInteraction = () => {
     const mock = {
-      reply: vi.fn(),
-      editReply: vi.fn(),
+      reply: vi.fn(() => Promise.resolve()),
+      editReply: vi.fn(() => Promise.resolve()),
     }
     return [mock as unknown as ChatInputCommandInteraction, mock] as const
   }
@@ -21,7 +22,7 @@ describe('reply', () => {
   it('should reply with the correct content', async () => {
     const [interaction, mock] = mockInteraction()
     const client = new MockClient()
-    reply('Hello, world!').call(client, interaction as unknown as ChatInputCommandInteraction, client)
+    reply('Hello, world!').call(client, interaction, client)
     expect(mock.reply).toHaveBeenCalledExactlyOnceWith({
       content: 'Hello, world!',
     })
@@ -30,7 +31,7 @@ describe('reply', () => {
   it('should edit reply with the correct content', async () => {
     const [interaction, mock] = mockInteraction()
     const client = new MockClient()
-    reply.edit('Hello, world!').call(client, interaction as unknown as ChatInputCommandInteraction, client)
+    reply.edit('Hello, world!').call(client, interaction, client)
     expect(mock.editReply).toHaveBeenCalledExactlyOnceWith({
       content: 'Hello, world!',
     })
@@ -39,10 +40,43 @@ describe('reply', () => {
   it('should reply with ephemeral flag', async () => {
     const [interaction, mock] = mockInteraction()
     const client = new MockClient()
-    reply.ephemeral('This is ephemeral!').call(client, interaction as unknown as ChatInputCommandInteraction, client)
+    reply.ephemeral('This is ephemeral!').call(client, interaction, client)
     expect(mock.reply).toHaveBeenCalledExactlyOnceWith({
       content: 'This is ephemeral!',
       flags: MessageFlags.Ephemeral,
+    })
+  })
+
+  it('should edit when deferred or replied', async () => {
+    const [interaction, mock] = mockInteraction()
+    interaction.deferred = true
+    const client = new MockClient()
+    reply('This is a deferred reply!').call(client, interaction, client)
+    expect(mock.editReply).toHaveBeenCalledExactlyOnceWith({
+      content: 'This is a deferred reply!',
+    })
+  })
+
+  it('should handle files in reply', async () => {
+    const [interaction, mock] = mockInteraction()
+    const client = new MockClient()
+    const file = 'file.txt'
+    // @ts-expect-error - this shouldn't error
+    reply.file(file).call(client, interaction, client)
+    expect(mock.reply).toHaveBeenCalledExactlyOnceWith({
+      files: [file],
+    })
+  })
+
+  it('should handle multiple files and content', async () => {
+    const [interaction, mock] = mockInteraction()
+    const client = new MockClient()
+    const files = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt']
+    const [f1, f2, ...rest] = files
+    reply.file(f1).file(f2).files(rest).send('Lots of files').call(client, interaction, client)
+    expect(mock.reply).toHaveBeenCalledExactlyOnceWith({
+      content: 'Lots of files',
+      files,
     })
   })
 })
